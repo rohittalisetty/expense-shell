3e220c2de2c6199c599e888b16482dcaf3000b2e
nagendra <rohittalisetty20@gmail.com>
2025-04-11 12:46:04 +0530

    expense shell

--- mysql.sh
+++ mysql.sh
@@ -42,5 +42,14 @@ VALIDATE $? "Installing MySQL Server"
 systemctl start mysqld &>>LOG_FILE_NAME
 VALIDATE $? "Starting MySQL Server"
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
-VALIDATE $? "Setting Root Password"
+mysql -h mysql.dawszone.space -u root -pExpenseApp@1 -e 'show databases;'
+
+if [ $? -ne 0 ]
+then
+    echo "MySQL Root password not setup" &>>LOG_FILE_NAME
+    mysql_secure_installation --set-root-pass ExpenseApp@1
+    VALIDATE $? "Setting Root Password"
+else
+    echo "MySQL Root password already setup ... $Y SKIPPING $N"
+fi      
+
